a81862eb15df09040a722480c962942fb1d376da
Hrui96 <1697049214@qq.com>
2022-03-28 16:16:43 +0800

    测试 1.0.0

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,25 +1,30 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<auto-parse-address boxClass="c-box" @result="result"></auto-parse-address>
 	</view>
 </template>
 
 <script>
-	import autoParseAddress from '../../uni_modules/'
+	import autoParseAddress from '../../uni_modules/R-autoParseAddress/components/R-autoParseAddress/R-autoParseAddress.vue'
 	export default {
+		components: {
+			'auto-parse-address': autoParseAddress
+		},
 		data() {
 			return {
-				title: 'Hello'
+				value: `收货人: 黄祥瑞
+手机号码: 18344050796
+所在地区: 广东省广州市番禺区大石街道
+详细地址: 广州番禺大山村湾低坊前街15号店`
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			result(obj) {
+				console.log('obj', obj);
+			}
 		}
 	}
 </script>
@@ -49,5 +54,9 @@
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
+	}
+	
+	.c-box {
+		min-height: 200upx; 
 	}
 </style>
